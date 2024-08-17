32bad053e4daa2e578da9abe003ec3b66e6ada53
ajaymarathe <ma143ajay@gmail.com>
2024-08-17 13:37:43 +0530

    minor ui & functional fixes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import Keyboard from "./Components/Keyboard";
 import WordBox from "./Components/WordBox";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  const [currentKey, setCurrentKeys] = useState("")
+  const [currentKey, setCurrentKeys] = useState("");
 
   const listOfWords = [
     "cat",
@@ -40,15 +41,19 @@ function App() {
   ];
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-8 mt-3">
-          <h4 className="text-center">10 Fast Figner</h4>
-          <WordBox words={listOfWords} setCurrentKeys={ setCurrentKeys } />
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8 mt-3">
+            <WordBox words={listOfWords} setCurrentKeys={setCurrentKeys} />
+          </div>
+          <div className="col-md-10">
+            <Keyboard currentKey={currentKey} />
+          </div>
         </div>
-        <Keyboard currentKey={ currentKey } />
       </div>
-    </div>
+    </>
   );
 }
 
